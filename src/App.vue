a96dc38c7c5953a44bb5e6b0f94ee3972fddd70d
Melrose <2713605684@qq.com>
2021-09-17 21:03:13 +0800

    feat:8/17学习setup的运用和其意义

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="12" />
+  <span>{{ name }}:{{ computedNameRef }}</span>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { computed, defineComponent, PropType, ref, watchEffect } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+interface Config {
+  name: string
+}
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
+  // props: {
+  //   age: {
+  //     // 注意这个 Number 不是 js 的写法，而是一个类，而这个类是需要用 ts 去定义的
+  //     type: Number as PropType<number>,
+  //   },
+  //   // config: {
+  //   //   type: Object as PropType<Config>,
+  //   //   required: true,
+  //   // },
+  // },
   components: {
     HelloWorld,
   },
-});
+  setup(props, { slots, attrs, emit }) {
+    let name = ref('jokcy')
+
+    setInterval(() => {
+      name.value += '1'
+    }, 1000)
+
+    const computedNameRef = computed(() => {
+      return name.value + '2'
+    })
+
+    watchEffect(() => {
+      // 会在这个函数引入的所有响应式的值变化之后重新执行（注意，只有是在这个函数引入的变量发生变化的时候才会触发）
+      console.log(name.value)
+    })
+
+    return {
+      name,
+      computedNameRef,
+    }
+  },
+})
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+// 也可以通过 createVNode 来替换掉 h 函数（对于 createVNode 来说， children 一定是数组， h 函数只是对 createVNode 进行了一些简单的封装，包括对参数的判断）
+import { createApp, defineComponent, h, createVNode } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+// // 如果在 .vue 的 template 里面，会根据相对地址，通过 vue.loader/url.loader 回去加载那个图片的地址形成真正的地址
+// // 样式也会不一样，因为 .vue 会通过 css.loader 来进行编译
+// // h 函数就是用来创建节点的，类似于 creatElement
+// const App = defineComponent({
+//     render() {
+//         return createVNode('div', { id: 'app' }, [
+//             createVNode('img', { alt: 'Vue logo', src: require('./assets/logo.png') },),
+//             createVNode(HelloWorld, { msg: 'Welcome to Your Vue.js + TypeScript App', age: 12 },)
+//         ])
+//     }
+// })
+
+createApp(App).mount('#app')
