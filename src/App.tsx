02a979f6f266fb7b2948083bce490992f961e3fd
Melrose <2713605684@qq.com>
2021-09-24 01:20:19 +0800

    feat: 新增页面编辑器的功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,56 @@
-import { defineComponent, reactive, ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref, Ref } from 'vue'
 
-function renderHelloWorld(num: number) {
-  return <HelloWorld age={num} />
+import MonacoEditor from './components/MonacoEditor'
+
+import { createUseStyles } from 'vue-jss'
+
+// 把 schema 这个对象转化成 string
+function toJson(data: any) {
+  // 需要传后续的两个参数
+  return JSON.stringify(data, null, 2)
 }
+
+const schema = {
+  type: 'string',
+}
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
 
 export default defineComponent({
   // 主张通过 setup 返回一个函数，因为 setup 声明的 ref reactive computed ... 都是可以在 return 的组件函数里面去使用的
   setup() {
-    const state = reactive({
-      name: 'Melrose',
-    })
+    const schemaRef: Ref<any> = ref(schema)
 
-    const numberRef = ref(1)
+    const handleCodeChange = (code: string) => {
+      let schema: any
+
+      try {
+        schema = JSON.parse(code)
+      } catch (error) {
+        console.log(error)
+      }
+      schemaRef.value = schema
+    }
+    const classesRef = useStyles()
 
     return () => {
-      const number = numberRef.value
+      const classes = classesRef.value
+      const code = toJson(schemaRef.value)
 
-      console.log(state.name)
-
-      // 在 jsx 所有的变量和表达式都通过 {} 来表示就可以了
-      // 注意在 jsx 里面当必传属性没有传的时候，或者传的类型不对，ts 都会提示报错
       return (
-        <div id="app">
-          <img src={require('./assets/logo.png')} alt="Vue logo" />
-          {/* <HelloWorld age={12} /> */}
-          <input type="text" v-model={state.name} />
-          {renderHelloWorld(12)}
-          <p>{state.name + number}</p>
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handleCodeChange}
+            title="Schema"
+            class={classes.editor}
+          />
         </div>
       )
     }
-
-    // return () => {
-    //     const number = numberRef.value
-    //     return h('div', { id: 'app' }, [
-    //         h('img', { alt: 'Vue logo', src: require('./assets/logo.png') },),
-    //         h(HelloWorld, { msg: 'Welcome to Your Vue.js + TypeScript App', age: 12 },),
-    //         h('p', state.name + number),
-    //     ])
-    // }
   },
 })
--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -2,4 +2,137 @@
 // vscode 用的这块编辑的区域实际上用的就是这个 monaco-editor,也是微软开源的代码编辑器的一个 js 的项目，如果最终要在页面展示这个 monaco-editor，展现 schema， ui-schema 还有 value ，这种对于我们这库的使用者想要去验证一些 schema 的展现的时候，就可以直接在这个页面上面去编辑它的 schema 以及 ui-schema ,及时的看到一种效果，对用户体验比较好一点。
 
 // vue-jss
-// css in js
+// css in js，可以直接在 js 中去写 css 的代码，并且可以给每个组件定义 css，从而防止了 css 和 js 产生割裂感
+
+/* eslint no-use-before-define: 0 */
+
+import {
+  defineComponent,
+  ref,
+  onMounted,
+  watch,
+  onBeforeUnmount,
+  shallowRef,
+} from 'vue'
+
+import * as Monaco from 'monaco-editor'
+
+import type { PropType } from 'vue'
+import { createUseStyles } from 'vue-jss'
+
+const useStyles = createUseStyles({
+  container: {
+    border: '1px solid #eee',
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: 5,
+  },
+  title: {
+    backgroundColor: '#eee',
+    padding: '10px 0',
+    paddingLeft: 20,
+  },
+  code: {
+    flexGrow: 1,
+  },
+})
+
+export default defineComponent({
+  props: {
+    code: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    onChange: {
+      type: Function as PropType<
+        (value: string, event: Monaco.editor.IModelContentChangedEvent) => void
+      >,
+      required: true,
+    },
+    title: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  setup(props) {
+    // must be shallowRef, if not, editor.getValue() won't work
+    const editorRef = shallowRef()
+
+    const containerRef = ref()
+
+    let _subscription: Monaco.IDisposable | undefined
+    let __prevent_trigger_change_event = false
+
+    onMounted(() => {
+      // 创建了一个 editor 而这个 editor 便是一个代码编辑器
+      const editor = (editorRef.value = Monaco.editor.create(
+        containerRef.value,
+        {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          value: props.code,
+          language: 'json',
+          formatOnPaste: true,
+          tabSize: 2,
+          minimap: {
+            enabled: false,
+          },
+        },
+      ))
+      // 会根据这个代码的内容的变化而实时的去返回给我们使用这个组件
+      _subscription = editor.onDidChangeModelContent((event) => {
+        if (!__prevent_trigger_change_event) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          props.onChange(editor.getValue(), event)
+        }
+      })
+    })
+
+    onBeforeUnmount(() => {
+      if (_subscription) _subscription.dispose()
+    })
+
+    watch(
+      () => props.code,
+      (v) => {
+        const editor = editorRef.value
+        const model = editor.getModel()
+        if (v !== model.getValue()) {
+          editor.pushUndoStop()
+          __prevent_trigger_change_event = true
+          // pushEditOperations says it expects a cursorComputer, but doesn't seem to need one.
+          model.pushEditOperations(
+            [],
+            [
+              {
+                range: model.getFullModelRange(),
+                text: v,
+              },
+            ],
+          )
+          editor.pushUndoStop()
+          __prevent_trigger_change_event = false
+        }
+        // if (v !== editorRef.value.getValue()) {
+        //   editorRef.value.setValue(v)
+        // }
+      },
+    )
+
+    const classesRef = useStyles()
+
+    return () => {
+      const classes = classesRef.value
+
+      return (
+        <div class={classes.container}>
+          <div class={classes.title}>
+            <span>{props.title}</span>
+          </div>
+          <div class={classes.code} ref={containerRef}></div>
+        </div>
+      )
+    }
+  },
+})
